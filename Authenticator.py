ac6318e8de370410b6503e6f9c4280da184f0743
David V Dalisay <dalisayd@athena.ecs.csus.edu>
2014-07-06 12:17:35 -0700

    adding authenticator.py

---
+++ Authenticator.py
@@ -0,0 +1,14 @@
+# Authenticator.py deals with user identification and whether the user is an admin or regular forum member
+
+# return "admin" or "member" if successfully authenticated
+# return False is unsuccessfully authenticated
+def authenticate(userName):
+	# if userName is in our database:
+		return "admin" #or "admin"
+	# else:
+		#return False
+
+def isAdmin(userName):
+	# TODO if userName has admin rights:
+	return True
+
